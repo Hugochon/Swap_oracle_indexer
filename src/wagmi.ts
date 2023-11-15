6d30795df5bf9fc262cc8fd5807ba1c25d9d8361
Hugochon <hugo.schneegans@gmail.com>
2023-11-15 23:49:44 +0100

    wagmi.ts tentative

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,12 +1,12 @@
 import { getDefaultWallets } from '@rainbow-me/rainbowkit'
-import { configureChains, createConfig } from 'wagmi'
+import { configureChains, createConfig, sepolia } from 'wagmi'
 import { goerli, mainnet } from 'wagmi/chains'
 import { publicProvider } from 'wagmi/providers/public'
 
 const walletConnectProjectId = '1061e8dc805953c3e99f708aacac0f11'
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [goerli],
+  [mainnet, goerli],
   [
     publicProvider(),
   ],
