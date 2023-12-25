2c67b0a360a970b9618a37ae75ebf62ffabea3c0
Hugochon <hugo.schneegans@gmail.com>
2023-12-25 16:28:10 +0100

    chainlink final commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,6 +32,8 @@ const Page: React.FC = () => {
     const [chainLinkPrice, setChainLinkPrice] = useState<any>([]);
     const [latestTimeStamp, setLatestTimeStamp] = useState<any>([]);
 
+    const [fetchingData, setFetchingData] = useState<boolean>(false);
+
     const { data: readChainLinkPrice, isLoading: readLoading, isError: readError } = useContractRead({
       address: MarketPlace_Contract.address,
       abi: MarketPlace_Contract.abi,
@@ -51,81 +53,168 @@ const Page: React.FC = () => {
         console.log(data);
       },
     });
-   
-  
 
-  return (
-    <div className="bg-purple-200 text-black min-h-screen">
-      <header className="bg-opacity-50 bg-purple-600 backdrop-blur-md text-white p-4">
-        <h1 className="text-3xl font-bold text-center text-black underline">
-          Marketplace test
-        </h1>
-        <div className="flex justify-between items-center p-2">
-          <ConnectButton />
-        </div>
-      </header>
+    const { write: writeMintUSDH } = useContractWrite({
+      ...StableCoin_Contract,
+      functionName: 'mint', // Make sure to use the correct function name for minting USDH
+      args: [BigInt(20000* 10 ** 18 )],
+    });
   
-      <Connected>
-      <div className="container mx-auto mt-8 grid md:grid-cols-3 gap-8">
-          <div className="mb-8 md:mb-0">
-            <h1 className="text-2xl font-bold mb-4 text-center">User Informations</h1>
-            <div>
-              <h3 className="text-lg text-gray-500">Current User Address : {userAddress}</h3>
-            </div>
-            <div className="bg-gold p-4 rounded mb-4">
-              <p className="text-xl text-center">
-                USDH Balance: <Balance token_Address={StableCoin_Contract.address} user_Address={userAddress} />
-              </p>
-            </div>
-            <div className="bg-ethlike p-4 rounded mb-4">
-            <p className="text-xl text-center text-white bg-color">
-              METH Balance : <Balance token_Address={ETHLike_Contract.address} user_Address={userAddress} />
-              </p>
-            </div>
+    const { write: writeMintMETH } = useContractWrite({
+      ...ETHLike_Contract,
+      functionName: 'mint', // Make sure to use the correct function name for minting METH
+      args: [BigInt(20 * 10 ** 18)],
+    });
+
+    const { write: writeTransaction } = useContractWrite({
+      ...MarketPlace_Contract,
+      functionName: 'getChainlinkDataFeedLatestAnswer', 
+    });
+
+    const handleFetchData = async () => {
+      try {
+        setFetchingData(true);
+        await writeTransaction();
+        // You may also want to refetch the latest timestamp here or update the UI accordingly
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setFetchingData(false);
+      }
+    };
+
+    const [timeDifference, setTimeDifference] = useState<number | null>(null);
+
+    useEffect(() => {
+      // Calculate time difference in minutes when readLatestTimeStamp is available
+      if (readLatestTimeStamp) {
+        const latestTimestampInSeconds = parseInt(readLatestTimeStamp.toString(), 10);
+        const currentTimestampInSeconds = Math.floor(Date.now() / 1000);
+        const differenceInSeconds = currentTimestampInSeconds - latestTimestampInSeconds;
+        const differenceInMinutes = Math.floor(differenceInSeconds / 60);
+  
+        setTimeDifference(differenceInMinutes);
+      }
+    }, [readLatestTimeStamp]);
+
+    const formattedUserAddress = userAddress || '0x00';
+  
+    const formattedChainLinkPrice = parseFloat(chainLinkPrice) * 10 ** -8;
+  
+    return (
+      <div className="bg-purple-200 text-black min-h-screen">
+        {/* Header Section */}
+        <header className="bg-opacity-50 bg-purple-600 backdrop-blur-md text-white p-4">
+          <h1 className="text-3xl font-bold text-center text-black underline">
+            Marketplace Test
+          </h1>
+          <div className="flex justify-between items-center p-2">
+            <ConnectButton />
           </div>
-          <div>
-              Latest ChainLink Price :   {chainLinkPrice}
+        </header>
+  
+        {/* Main Content Section */}
+        <Connected>
+          <div className="container mx-auto mt-8 grid md:grid-cols-3 gap-8">
+            {/* User Information Section */}
+            <div className="mb-8 md:mb-0">
+              <h1 className="text-2xl font-bold mb-4 text-center">User Information</h1>
+              <div className="mb-4">
+                <h3 className="text-lg text-gray-500">Current User Address:</h3>
+                <p className="text-sm">{formattedUserAddress}</p>
+              </div>
+              <div className="bg-gold p-4 rounded mb-4">
+                <p className="text-xl text-center">
+                  USDH Balance: <Balance token_Address={StableCoin_Contract.address} user_Address={formattedUserAddress} />
+                </p>
+              </div>
+              <div className="bg-ethlike p-4 rounded mb-4">
+                <p className="text-xl text-center text-white bg-color">
+                  METH Balance: <Balance token_Address={ETHLike_Contract.address} user_Address={formattedUserAddress} />
+                </p>
+              </div>
+              <p className="text-center mb-4 text-gray-600">Not enough USDH or Meth? Mint some!</p>
+              <div className="flex justify-around mt-4">
+                <button
+                  onClick={() => writeMintUSDH()}
+                  className="bg-gold text-white px-4 py-2 rounded"
+                >
+                  <span className="text-black">Mint USDH</span>
+                </button>
+                <button
+                  onClick={() => writeMintMETH()}
+                  className="bg-ethlike text-white px-4 py-2 rounded"
+                >
+                  Mint METH
+                </button>
+              </div>
+            </div>
+  
+            {/* ChainLink Information Section */}
+            <div>
+              <h1 className="text-2xl font-bold mb-4 text-center">ChainLink Information</h1>
               <br></br>
-              Latest TimeStamp : {latestTimeStamp}
-          </div>
-          <div>
-            <h1 className="text-2xl font-bold mb-4 text-center">Marketplace Informations</h1>
-            <div>
-              <h3 className="text-lg text-gray-500">Marketplace Contract Address : {MarketPlace_Contract.address}</h3>
+              <br></br>
+              <div className="bg-gray-100 p-6 rounded mb-6">
+                <div className="flex flex-col items-center">
+                  <p className="text-xl mb-4">
+                    Latest ChainLink Price: <span className="text-ethlike underline">${formattedChainLinkPrice.toFixed(2)}</span>
+                  </p>
+                  <p className="text-xl mb-4">
+                    Latest Time Actualization: <span className="text-ethlike">{timeDifference} minutes ago</span>
+                  </p>
+                  <button
+                    onClick={handleFetchData}
+                    className="bg-purple-600 text-white px-4 py-2 rounded"
+                    disabled={fetchingData}
+                  >
+                    {fetchingData ? 'Fetching Data...' : 'Fetch Data'}
+                  </button>
+                </div>
+              </div>
             </div>
-            <div className="bg-gold p-4 rounded mb-4">
-              <p className="text-xl text-center">
-              Marketplace USDH Balance: <Balance token_Address={StableCoin_Contract.address} user_Address={MarketPlace_Contract.address} />
-              </p>
-            </div>
-            <div className="bg-ethlike p-4 rounded mb-4">
-              <p className="text-xl text-center text-white bg-color">
-              Marketplace METH Balance : <Balance token_Address={ETHLike_Contract.address} user_Address={MarketPlace_Contract.address} />
-              </p>
-            </div>
-          </div>
-        </div>
   
-        <div className="flex justify-center mt-8">
-          <BuySellMegaETHComponent />
-          <ApproveButton
-            token_Address={StableCoin_Contract.address}
-            user_Address={userAddress}
-            spender_Address={MarketPlace_Contract.address}
-            spender_Allowance={BigInt(10 * 10 ** 18)}
-            token_Symbol='USDH'
-          ></ApproveButton>
-          <ApproveButton
-            token_Address={ETHLike_Contract.address}
-            user_Address={userAddress}
-            spender_Address={MarketPlace_Contract.address}
-            spender_Allowance={BigInt(10 * 10 ** 18)}
-            token_Symbol='METH'
-          ></ApproveButton>
-        </div>
-      </Connected>
-    </div>
-  );
-}
-
-export default Page;
+            {/* Marketplace Information Section */}
+            <div>
+              <h1 className="text-2xl font-bold mb-4 text-center">Marketplace Information</h1>
+              <div className="mb-4">
+                <h3 className="text-lg text-gray-500">Marketplace Contract Address:</h3>
+                <p className="text-sm">{MarketPlace_Contract.address}</p>
+              </div>
+              <div className="bg-gold p-4 rounded mb-4">
+                <p className="text-xl text-center">
+                  Marketplace USDH Balance: <Balance token_Address={StableCoin_Contract.address} user_Address={MarketPlace_Contract.address} />
+                </p>
+              </div>
+              <div className="bg-ethlike p-4 rounded mb-4">
+                <p className="text-xl text-center text-white bg-color">
+                  Marketplace METH Balance: <Balance token_Address={ETHLike_Contract.address} user_Address={MarketPlace_Contract.address} />
+                </p>
+              </div>
+            </div>
+          </div>
+  
+          {/* Actions Section */}
+          <div className="flex justify-center mt-8">
+            <BuySellMegaETHComponent meth_price={formattedChainLinkPrice} />
+            <ApproveButton
+              token_Address={StableCoin_Contract.address}
+              user_Address={formattedUserAddress}
+              spender_Address={MarketPlace_Contract.address}
+              spender_Allowance={BigInt(10 * 10 ** 18)}
+              token_Symbol='USDH'
+            />
+            <ApproveButton
+              token_Address={ETHLike_Contract.address}
+              user_Address={formattedUserAddress}
+              spender_Address={MarketPlace_Contract.address}
+              spender_Allowance={BigInt(10 * 10 ** 18)}
+              token_Symbol='METH'
+            />
+          </div>
+        </Connected>
+      </div>
+    );
+  }
+  
+  export default Page;
--- src/components/Buy&Sell_MegaETH.tsx
+++ src/components/Buy&Sell_MegaETH.tsx
@@ -1,34 +1,65 @@
-import React, { useState } from 'react';
+import React, { use, useEffect, useState } from 'react';
 import { useContractWrite, useContractRead } from 'wagmi';
 
 import { MarketPlace_Contract } from './contracts';
 
-export function BuySellMegaETHComponent() {
-  const [amount, setAmount] = useState("10000");
-  const [isBuying, setIsBuying] = useState<boolean>(true); // Track whether it's buying or selling
+interface BuySellMegaETHProps {
+  meth_price: number;
+}
 
+export function BuySellMegaETHComponent(props: BuySellMegaETHProps) {
+  const [amount, setAmount] = useState("0");
+  const [isBuying, setIsBuying] = useState<boolean>(true);
+  const [expectedValue, setExpectedValue] = useState<number>(0);
+  const [errorDisplay, SetErrorDisplay] = useState<boolean>(false);
+  const [error, setError] = useState<string>('');
 
-  // if (isNaN(amount)) {
-  //   setAmount(10000);
-  // }
-
-  const { write: writeTransaction } = useContractWrite({
+  const { write: writeTransaction,error:reverterror } = useContractWrite({
     ...MarketPlace_Contract,
-    functionName: isBuying ? 'buy_MegaETH' : 'sell_MegaETH', // Use buy or sell function based on the mode
-    args: [amount ? BigInt(parseInt(amount) * 10**18) : BigInt(0) ], // Update arguments as needed
+    functionName: isBuying ? 'buy_MegaETH' : 'sell_MegaETH',
+    args: [amount ? BigInt(parseInt(amount) * 10 ** 18) : BigInt(0)],
   });
+
+  // Calculate expected value based on mode and ETH price
+  const calculateExpectedValue = () => {
+    const ethAmount = parseInt(amount);
+    if(amount === "") {
+      setExpectedValue(0);
+      return;
+    }
+    setExpectedValue(ethAmount * props.meth_price);
+  };
 
   const handleTransaction = async () => {
     try {
-      // Call the write function to buy or sell MegaETH based on the mode
       await writeTransaction();
-
-      // Optionally, you can refresh data after the transaction is successful
-      // await refetchData(); // Implement a function to fetch the updated data
+     
     } catch (error) {
       console.error(`Error ${isBuying ? 'buying' : 'selling'} MegaETH:`, error);
     }
   };
+
+  useEffect(() => {
+    calculateExpectedValue();
+  }, [amount, isBuying]);
+
+  useEffect(() => {
+    // Use this effect to handle changes in the error state
+    if (reverterror !== null) {
+      SetErrorDisplay(true);
+  
+      // Extract the reason from the error message
+      const errorMessage = reverterror.message || '';
+      const startIndex = errorMessage.indexOf('reverted with the following reason:') + 'reverted with the following reason:'.length;
+      const endIndex = errorMessage.indexOf('Contract Call');
+      const reason = errorMessage.substring(startIndex, endIndex).trim();
+  
+      setError(`Error: ${reason}`);
+    } else {
+      SetErrorDisplay(false);
+    }
+  }, [reverterror]);
+  
 
   return (
     <div className="bg-gray-100 p-4 rounded mb-4">
@@ -51,14 +82,27 @@ export function BuySellMegaETHComponent() {
           {isBuying ? 'Buy' : 'Sell'} MegaETH
         </button>
         <button
-          onClick={() => setIsBuying(!isBuying)}
+          onClick={() => {
+            setIsBuying(!isBuying);
+            calculateExpectedValue(); // Recalculate expected value on mode switch
+          }}
           className="bg-purple-600 text-white px-4 py-2 rounded"
         >
           Switch to {isBuying ? 'Sell' : 'Buy'} Mode
         </button>
       </div>
+      {  (
+        <div className="text-center">
+          <p className="text-xl">
+            Expected USDH Price: <span className="text-gold">${expectedValue.toFixed(0)}</span>
+          </p>
+          {errorDisplay && (
+            <div className="text-red-500 text-sm mt-2">{error}</div>
+          )}
+        </div>
+      )}
     </div>
-  );  
+  );
 }
 
 export default BuySellMegaETHComponent;
